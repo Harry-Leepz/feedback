56403cbe1c5649fa061774e8a788382235058faf
Harry Dhillon <hdhillon478@gmail.com>
2022-12-11 20:02:41 +0000

    Add Header component to App

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='container'>
+      <h1>Feedback UI</h1>
+    </div>
+  );
 };
 
 export default Header;
