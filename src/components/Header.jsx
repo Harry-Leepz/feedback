a4ebb67a55f99edca969ee9e7877d5e17491245e
Harry Dhillon <hdhillon478@gmail.com>
2022-12-12 21:46:48 +0000

    Add header styling variable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+const headerStyles = {
+  backgroundColor: "rgba(0,0,0,0.4)",
+  color: "#ff6a95",
+};
 
 const Header = () => {
   return (
-    <div className='container'>
+    <div className='container' style={headerStyles}>
       <h1>Feedback UI</h1>
     </div>
   );
