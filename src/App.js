dabf4248655d1d31d9cd3a2a1955edcb6478332d
Harry Dhillon <hdhillon478@gmail.com>
2022-12-12 21:57:20 +0000

    Add FeedbackItem to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <div className='container'>
+        <FeedbackItem />
+      </div>
     </div>
   );
 }
