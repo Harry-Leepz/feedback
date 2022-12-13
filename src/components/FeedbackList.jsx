8503b4dad9d4c483b217c4088b0cebe89c730e28
Harry Dhillon <hdhillon478@gmail.com>
2022-12-13 21:59:15 +0000

    Build FeedbackList component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,21 @@
-const FeedbackList = () => {
-  return <div>FeedbackList</div>;
+import FeedbackItem from "./FeedbackItem";
+
+const FeedbackList = ({ data }) => {
+  if (!data || data.length === 0) {
+    return <p>No Feedback Yet...</p>;
+  }
+
+  return (
+    <div className='feedback-list'>
+      {data.map((feedback) => (
+        <FeedbackItem
+          key={feedback.id}
+          rating={feedback.rating}
+          text={feedback.text}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default FeedbackList;
