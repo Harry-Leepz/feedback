f6ef278234cc6d8ac1e1ab018898f5bb1fcdd1e1
Harry Dhillon <hdhillon478@gmail.com>
2022-12-13 21:45:03 +0000

    Import feedbackData into app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackItem from "./components/FeedbackItem";
 
+import feedbackData from "./data/FeedbackData";
+
 function App() {
+  const [feedback, setFeedback] = useState(feedbackData);
   return (
     <div className='App'>
       <Header />
