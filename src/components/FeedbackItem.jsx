f51d24b17177c198d437a716eac9925f18316bf1
Harry Dhillon <hdhillon478@gmail.com>
2022-12-14 21:56:17 +0000

    Add react-icon to FeedbackItem component

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,13 @@
 import Card from "./shared/Card";
+import { FaTimesCircle } from "react-icons/fa";
 
 const FeedbackItem = ({ text, rating }) => {
   return (
     <Card>
       <div className='num-display'>{rating}</div>
+      <button className='close'>
+        <FaTimesCircle color='black' />
+      </button>
       <div className='text-display'>{text}</div>
     </Card>
   );
