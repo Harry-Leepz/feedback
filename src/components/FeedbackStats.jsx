7e49fa158392cd498a913c1a34fa502cb0dacefd
Harry Dhillon <hdhillon478@gmail.com>
2022-12-18 20:27:30 +0000

    Add logic to show rating average

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+const FeedbackStats = ({ data }) => {
+  // Calculate average ratings
+  let average =
+    data.reduce((acc, current) => {
+      return acc + current.rating;
+    }, 0) / data.length;
 
-const FeedbackStats = () => {
-  return <div>FeedbackStats</div>;
+  // set avergae to one deciaml place and
+  // have any .0 replace by a single figure
+  average = average.toFixed(1).replace(/[.,]$0/, "");
+
+  return (
+    <div className='feedback-stats'>
+      <h4> {data.length} Reviews</h4>
+      <h4> Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  );
 };
 
 export default FeedbackStats;
