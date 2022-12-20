226399e74463340b3b0cde07442d42fdada47ae7
Harry Dhillon <hdhillon478@gmail.com>
2022-12-20 19:48:38 +0000

    Start building FeedbackForm component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import Card from "../components/shared/Card";
+
 const FeedbackForm = () => {
-  return <div>FeedbackForm</div>;
+  const [text, setText] = useState("");
+
+  const textInputHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us ?</h2>
+        <div className='input-group'>
+          <input
+            onChange={textInputHandler}
+            value={text}
+            type='text'
+            placeholder='Write a review'
+          />
+          <button type='submit'>Send</button>
+        </div>
+      </form>
+    </Card>
+  );
 };
 
 export default FeedbackForm;
