e2e0ea307794785d9b5226f7a8aa5a6cf2803143
Harry Dhillon <hdhillon478@gmail.com>
2023-07-15 16:01:55 +0100

    Add logic to validate text input on feedbackform

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,9 +4,23 @@ import Button from "./shared/Button";
 
 const FeedbackForm = () => {
   const [text, setText] = useState("");
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const textInputHandler = (event) => {
     setText(event.target.value);
+
+    // check text input and set error message and submit button disbaled attribute
+    if (text === "") {
+      setMessage("");
+      setIsDisabled(true);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be atleast 10 characters!");
+      setIsDisabled(true);
+    } else {
+      setMessage("");
+      setIsDisabled(false);
+    }
   };
 
   return (
@@ -20,10 +34,11 @@ const FeedbackForm = () => {
             type='text'
             placeholder='Write a review'
           />
-          <Button type='submit' disabled={false} version={"primary"}>
+          <Button type='submit' isDisabled={isDisabled} version={"primary"}>
             Send
           </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
