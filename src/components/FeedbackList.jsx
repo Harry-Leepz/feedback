d8d2cb107ebd21cfe0346f71b89bfd8a0c5625ab
Harry Dhillon <hdhillon478@gmail.com>
2023-07-23 19:27:13 +0100

    Add framer motion animation to feedback list

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,7 @@
 import FeedbackItem from "./FeedbackItem";
 
+import { motion, AnimatePresence } from "framer-motion";
+
 const FeedbackList = ({ data, onDeleteItem }) => {
   if (!data || data.length === 0) {
     return <p>No Feedback Yet...</p>;
@@ -7,11 +9,32 @@ const FeedbackList = ({ data, onDeleteItem }) => {
 
   return (
     <div className='feedback-list'>
-      {data.map((item) => (
-        <FeedbackItem key={item.id} item={item} onDeleteItem={onDeleteItem} />
-      ))}
+      <AnimatePresence>
+        {data.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              onDeleteItem={onDeleteItem}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {data.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} onDeleteItem={onDeleteItem} />
+  //     ))}
+  //   </div>
+  // );
 };
 
 export default FeedbackList;
