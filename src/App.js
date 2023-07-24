0db6e37b8c8a1bec402b2fbbedb23bef9f5d5432
Harry Dhillon <hdhillon478@gmail.com>
2023-07-24 21:56:40 +0100

    Add more text content to the about component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid"; // generate custom id for adding new feedback
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import About from "./pages/About";
 
 import feedbackData from "./data/FeedbackData";
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Card from "../components/shared/Card";
 
 const About = () => {
   return (
-    <>
+    <Card>
       <h1>This is the About Us Page!</h1>
-    </>
+      <p>This is react app to leave feedback about a product or service.</p>
+      <p>Version: 1.0.0</p>
+    </Card>
   );
 };
 
