ea41beb3dafb30032ab7a43b019b08a1e8d23d92
Harry Dhillon <hdhillon478@gmail.com>
2023-07-25 09:14:04 +0100

    Add home link to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -7,6 +8,7 @@ const About = () => {
       <h1>This is the About Us Page!</h1>
       <p>This is react app to leave feedback about a product or service.</p>
       <p>Version: 1.0.0</p>
+      <Link to='/'>Back Home</Link>
     </Card>
   );
 };
