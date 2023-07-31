e2745ced47b904df1cc10fc008f8988364680f3d
Harry Dhillon <hdhillon478@gmail.com>
2023-07-31 14:54:12 +0100

    Update feedback context with feedback edit state

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,16 +1,15 @@
 import { useState, createContext } from "react";
 import { v4 as uuidv4 } from "uuid"; // generate custom id for adding new feedback
+import FeedbackData from "../data/FeedbackData";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This is from the context",
-      rating: 10,
-    },
-  ]);
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
 
   // delete a feedback item
   const deleteFeedback = (id) => {
@@ -21,10 +20,19 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
+  // add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     // using spread operator to add the new feedback to the previous state
     setFeedback([newFeedback, ...feedback]);
+  };
+
+  // edit a single feedback
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
   };
 
   return (
@@ -33,6 +41,7 @@ export const FeedbackProvider = ({ children }) => {
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}
