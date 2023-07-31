1272c4646d70e944d303589ef6683559edbd99da
Harry Dhillon <hdhillon478@gmail.com>
2023-07-31 14:54:40 +0100

    Update feedback item with edit feedback context

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,16 +1,19 @@
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
 import Card from "./shared/Card";
-import { FaTimesCircle } from "react-icons/fa";
+import { FaTimes, FaEdit } from "react-icons/fa";
 
 const FeedbackItem = ({ item }) => {
-  const { deleteFeedback } = useContext(FeedbackContext);
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
 
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
       <button onClick={() => deleteFeedback(item.id)} className='close'>
-        <FaTimesCircle color='black' />
+        <FaTimes color='purple' />
+      </button>
+      <button onClick={() => editFeedback(item)} className='edit'>
+        <FaEdit color='purple' />
       </button>
       <div className='text-display'>{item.text}</div>
     </Card>
